e79d8e9a60545a52468013719129bd806f8e7bf3
Angela Zhang <zhangela@mit.edu>
2014-04-18 19:26:05 -0400

    suffix

--- fakeword.py
+++ fakeword.py
@@ -4,8 +4,10 @@ import os, sys
 vowels = ["a", "e", "i", "o", "u"]
 consonants = [
                 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 
-                'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 
-                'w', 'x', 'y', 'z',
+                'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 
+                'w', 'x', 'y', 'z'
+            ]
+prefixes = [
                 'ab', 'abs', 'ac', 'acr', 'ad', 'ag', 
                 'al', 'aer', 'am', 'amb', 'an', 'ann', 
                 'ant', 'ap', 'ar', 'arct', 'astr', 'aur', 
@@ -19,13 +21,19 @@ consonants = [
                 'id', 'in', 'im', 'int', 'intr', 'is', 'jac',
                 'kl', 'lab', 'lax', 'lat', 'lev', 'lin', 'mal',
                 'mic', 'mon', 'nar', 'nov', 'oct', 'ob', 'os',
-                'ol', 'op', 'or', 'os', 'ov', 'ot', 'ph'
+                'ol', 'op', 'or', 'os', 'ov', 'ot', 'ph', 'qu'
+            ]
+
+suffixes = [
+                'y','ful', 'ly', 'ble', 'al', 'ed', 'en', 'er', 'est', 
+                'ic', 'ing', 'ic', 'ing', 'tion', 'ty', 'ive',
+                'ment', 'ness', 'es'
             ]
 
 
 def _assign_weight(arr):
     weighted = []
-    length = len(arr)
+    length = max(len(arr), 20)
     for index, letter in enumerate(arr):
          weighted.append((letter, length - index))
     population = [val for val, cnt in weighted for i in range(cnt)]
@@ -33,12 +41,16 @@ def _assign_weight(arr):
 
 weighted_vowels = _assign_weight(vowels)
 weighted_consonants = _assign_weight(consonants)
+weighted_prefixes = _assign_weight(prefixes)
 
 def _vowel():
     return random.choice(weighted_vowels)
  
 def _consonant():
     return random.choice(weighted_consonants)
+
+def _prefix():
+    return random.choice(weighted_prefixes)
  
 def _cv():
     return _consonant() + _vowel()
@@ -48,13 +60,18 @@ def _cvc():
  
 def _syllable():
     return random.choice([_vowel, _cv, _cvc])()
+
+def _pre():
+    return random.choice([_vowel, _cv, _cvc, _prefix])()
  
 def create_fake_word():
     """ This function generates a fake word by creating between two and three
         random syllables and then joining them together.
     """
     syllables = []
-    for x in range(random.randint(2,3)):
+    syllables.append(_pre())
+    syllables.append(_vowel())
+    for x in range(random.randint(0,1)):
         syllables.append(_syllable())
     return "".join(syllables)
  
